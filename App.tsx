bb762d9bdce7a78b0d37c118c9bc93e5b88353b2
Bruno <118033210+brunoderosa@users.noreply.github.com>
2025-12-23 19:43:06 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,41 +1,9 @@
-import React, { useEffect } from 'react';
-import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import About from './pages/About';
-import Services from './pages/Services';
-import Contact from './pages/Contact';
-import WhatsAppButton from './components/WhatsAppButton';
-import { RoutePath } from './types';
-
-const ScrollToTop = () => {
-  const { pathname } = useLocation();
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
-  return null;
-};
-
-const App: React.FC = () => {
+function App() {
   return (
-    <Router>
-      <ScrollToTop />
-      <div className="min-h-screen flex flex-col bg-white overflow-x-hidden">
-        <Navbar />
-        <main className="flex-grow">
-          <Routes>
-            <Route path={RoutePath.Home} element={<Home />} />
-            <Route path={RoutePath.About} element={<About />} />
-            <Route path={RoutePath.Services} element={<Services />} />
-            <Route path={RoutePath.Contact} element={<Contact />} />
-          </Routes>
-        </main>
-        <Footer />
-        <WhatsAppButton />
-      </div>
-    </Router>
+    <div style={{ color: 'white', fontSize: '24px' }}>
+      TESTE JESSICA ROSA
+    </div>
   );
-};
+}
 
 export default App;
