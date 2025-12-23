c6de0e265a4bc64a0aa138cca254cc8a638ad3a2
Bruno <118033210+brunoderosa@users.noreply.github.com>
2025-12-23 19:47:12 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/JessicaRosa/', // importante para GitHub Pages
+    base: '/JessicaRosa/', // para GitHub Pages
     server: {
       port: 3000,
       host: '0.0.0.0',
